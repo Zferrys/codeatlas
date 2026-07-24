173388866c9964c4e781422ebf2f6422e2ae002f
zferry <1978738217@qq.com>
2026-07-24 23:36:07 +0800

    修复：显式注册 @Primary JDBC DataSourceTransactionManager

--- codeatlas-server/src/main/java/com/codeatlas/server/config/TransactionManagerConfig.java
+++ codeatlas-server/src/main/java/com/codeatlas/server/config/TransactionManagerConfig.java
@@ -1,25 +1,24 @@
 package com.codeatlas.server.config;
 
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.TransactionManagementConfigurer;
+
+import javax.sql.DataSource;
 
 /**
- * 显式指定默认事务管理器为 JDBC (MySQL)，防止 @Transactional 无参时误选 Neo4j。
+ * 显式创建 JDBC 事务管理器并标记为 @Primary。
+ * 原因：Neo4j 的 Neo4jTransactionManager 先于 DataSourceTransactionManager 注册，
+ * 导致 @ConditionalOnMissingBean 跳过 JDBC 事务管理器，@Transactional 无参时全部走 Neo4j。
  */
 @Configuration
-public class TransactionManagerConfig implements TransactionManagementConfigurer {
+public class TransactionManagerConfig {
 
-    private final PlatformTransactionManager txManager;
-
-    public TransactionManagerConfig(
-            @Qualifier("transactionManager") PlatformTransactionManager txManager) {
-        this.txManager = txManager;
-    }
-
-    @Override
-    public PlatformTransactionManager annotationDrivenTransactionManager() {
-        return txManager;
+    @Primary
+    @Bean
+    public PlatformTransactionManager transactionManager(DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
     }
 }
